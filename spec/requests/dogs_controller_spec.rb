6a9d4b53c48f6330cd62be8c06f80bb24d42cfb6
suimi34 <47468463+suimi34@users.noreply.github.com>
2024-04-04 22:18:31 +0900

    add rspec (#35)
    * add. gem rspec
    
    * add. test
    
    * add test libraries
    
    * add. graphql test
    
    * add schema test

---
+++ spec/requests/dogs_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe "Dogs", type: :request do
+  describe "idnex" do
+    let!(:dog) { create(:dog) }
+
+    it "returns 200" do
+      get dogs_path
+      expect(response).to have_http_status(200)
+    end
+  end
+end
