95160bc74cc6013719a9c18129e87ed1aef2e06b
Susumu Arimoto <susumu2934@gmail.com>
2025-05-04 15:05:07 +0900

    upadte amazon sqs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
-  config.active_job.queue_adapter = :sqs
+  config.active_job.queue_adapter = :amazon_sqs
   # config.active_job.queue_name_prefix = "sandbox_production"
 
   config.action_mailer.perform_caching = false
